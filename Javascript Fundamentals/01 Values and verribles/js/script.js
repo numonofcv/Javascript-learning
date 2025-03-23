61abcfd699bdb5ea65f5c00db18620b396204ad1
Numonof <numonofcv@gmail.com>
2025-03-23 12:50:56 +0300

    Values and Variables

---
+++ Javascript Fundamentals/01 Values and verribles/js/script.js
@@ -0,0 +1,23 @@
+let firstJavackriptCode = "Hello world";
+console.log(firstJavackriptCode);
+
+console.log("Jonas");
+console.log(23);
+let firstName = "Bob" 
+console.log(firstName );
+console.log(firstName );
+console.log(firstName );
+// let jonas&matilda='Jonas Matilda'; Unexpected token '&'
+let jonas_matilda='Jonas Matilda'; 
+// let new = 3;Unexpected token 'new'
+// let function = 'abs';Unexpected token 'function'
+// let 3years = 3; syntaxError : invalid
+// let PI = 3.1415; let pi=3.1415;
+
+let myFirstJob = 'programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'prorammer'
+let job2 = 'teacher'
+
+console.log(myFirstJob);
