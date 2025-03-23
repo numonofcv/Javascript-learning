d9431f7f55ad31a229aabc66cbeaf5afffea38a0
Numonof <numonofcv@gmail.com>
2025-03-23 14:06:15 +0300

    Data types in javascript

---
+++ Javascript Fundamentals/02 Data types/js/script.js
@@ -0,0 +1,21 @@
+let javascriptIsFun=true;
+console.log(javascriptIsFun);
+
+console.log(typeof false); // boolen
+console.log(typeof true); // boolen
+console.log(typeof javascriptIsFun); // boolen
+console.log(typeof 23); // number
+console.log(typeof "Tomas"); // string
+
+javascriptIsFun = 'Yes';
+console.log(typeof javascriptIsFun); // string
+let year;
+console.log(year);
+console.log(typeof year); //undefined
+
+
+year = 1991;
+console.log(year);
+console.log(typeof year); // number
+
+console.log(typeof null); // object
