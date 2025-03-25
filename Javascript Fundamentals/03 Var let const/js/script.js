a5c8da3d1a44f08006e9279707376d4a4443d468
Numonof <numonofcv@gmail.com>
2025-03-25 16:22:18 +0300

    Var, let, const

---
+++ Javascript Fundamentals/03 Var let const/js/script.js
@@ -0,0 +1,26 @@
+let age = 23;
+console.log(age);
+age = 25;
+console.log(age);
+// console.log(birthYear); ReferenceError
+const birthYear = 2002;
+console.log(birthYear);
+// birthYear = 2000; //  TypeError
+// console.log(age); // TypeError
+
+// const job; //SyntaxError
+// console.log(job);
+
+// console.log(firstName); // ReferranceError
+let firstName;
+console.log(firstName); //undefined
+firstName = 'John';
+
+console.log(job); //undefined
+var job = 'programmer';
+job = 'teacher';
+// console.log(job);
+
+// console.log(lastName); // ReferenceError
+lastName = "Smit";
+console.log(lastName);
