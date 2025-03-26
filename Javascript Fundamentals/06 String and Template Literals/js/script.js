e9961b81334c4de4e5322070914e84a077f7a1cd
Numonof <numonofcv@gmail.com>
2025-03-26 15:42:06 +0300

    String and Template Literals

---
+++ Javascript Fundamentals/06 String and Template Literals/js/script.js
@@ -0,0 +1,20 @@
+const firstName = 'Jonas';
+const job = 'Teacher';
+const birthYear = 1991;
+const year = 2037;
+ 
+const jonas = "I'm" + ' '+ firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas);
+
+
+const jonasNew = `My name is ${firstName} and I'm ${year - birthYear} years old, My job is ${job} `;
+
+// const jonasJob = `my job is ${job}`;
+
+console.log(jonasNew);
+
+
+console.log(`Just regular string....`);
+console.log(`String with  
+multiple 
+line`);
