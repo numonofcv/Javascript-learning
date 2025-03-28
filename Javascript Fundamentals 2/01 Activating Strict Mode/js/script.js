09fe7877b344805d462787fd5e53e49c02ff5785
Numonof <numonofcv@gmail.com>
2025-03-28 14:52:50 +0300

    Activating Strict Mode

---
+++ Javascript Fundamentals 2/01 Activating Strict Mode/js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let hasDriverLicanese = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriverLicanese = true;
+}
+if (hasDriverLicanese) {
+    console.log(' I can drive :D');
+}
+
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
