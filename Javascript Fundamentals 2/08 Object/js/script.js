bbd6495555702ee6febc13bea5ab22f8dabd7b73
Numonof <numonofcv@gmail.com>
2025-04-10 01:03:55 +0300

    Object

---
+++ Javascript Fundamentals 2/08 Object/js/script.js
@@ -0,0 +1,14 @@
+const jonasArray = [
+    'Jonas', 'Schmedtmann', 2037 - 1997,
+    'teacher',
+    [
+        'Michael', 'Stiven', 'Peter'
+    ]
+];
+const jonas = {
+    firstname: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1997,
+    job: 'teacher',
+    friends: ['Michael', 'Stiven', 'Peter']
+};
