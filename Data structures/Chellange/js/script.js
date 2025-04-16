20a7830cd6f662350d5793ce62c2dabc35adf8d1
Numonof <numonofcv@gmail.com>
2025-04-16 03:13:54 +0300

    CHALLENGE

---
+++ Data structures/Chellange/js/script.js
@@ -0,0 +1,50 @@
+'use strict';
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borussia Dortmund',
+    players: [
+      [ // players[0] — Bayern Munich o'yinchilari
+        'Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies',
+        'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski'
+      ],
+      [ // players[1] — Borussia Dortmund o'yinchilari
+        'Burki', 'Schulz', 'Hummels', 'Akanji', 'Hakimi',
+        'Weigl', 'Witsel', 'Hazard', 'Brandt', 
+      ]
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5
+    }
+  };
+
+  const [players1, players2] = game.players;
+  console.log(players1, players2);
+
+  const [gk, ...fieldplayer] = players1;
+  console.log(gk, fieldplayer);
+  
+  const allPlayers = [...players1, ...players2]
+  console.log(allPlayers);
+  
+  const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic']
+  console.log(players1Final);
+
+  const {odds:{team1, x:draw, team2}}= game;
+console.log(team1, draw, team2);
+
+  const printGoals = function(...players){
+    console.log(players);
+    
+    console.log(`${players.length} goals were scored`);
+  };
+  printGoals('Davies', 'Muller', ' Lewandowskiy', 'Kimmich');
+  printGoals('Davies', 'Muller',);
+  printGoals(...game.scored);
+
+  team1<team2 && console.log('Team 1 is more likely to win') 
+  team1>team2 && console.log('Team 2 is more likely to win') 
