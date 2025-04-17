2bdb79e9f73db1cbbbc04fcae6291fdb453ccb52
Numonof <numonofcv@gmail.com>
2025-04-17 04:00:29 +0300

    Working with string Part 1

---
+++ Data structures/Working with string/js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+const airline = 'TAP Air Portugal';
+const plane = 'A320'
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[0]);
+console.log('B737'[0]);
+
+// String.length
+console.log(airline.length);
+console.log('B737'.length);
+
+// index of
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+// cut with slice() 
+console.log(airline.slice(4, 7));
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.indexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+    // B and E are middle seats
+    const s = seat.slice(-1);
+    if(s==='B'|| s === 'E'){
+        console.log('You got the middle seat😬')
+    }else{
+        console.log('You got lucky 😎');
+    }
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+
+console.log(new String('jonas'))
+console.log(typeof new String('jonas'))
+console.log(typeof String('jonas'))
+console.log(typeof new String('jonas').slice(1))
