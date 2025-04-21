75b427cfd0578c5348eb424751caac5bfb23012e
Numonof <numonofcv@gmail.com>
2025-04-21 03:10:56 +0300

    The New at  Mathod

---
+++ Working with Arrays/The New at Mathods/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+const arr = [23, 11, 64];
+console.log(arr)
+console.log(arr.at(0));
+
+// getting last array //
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-2)[0]);
+console.log(arr.at(-2));
