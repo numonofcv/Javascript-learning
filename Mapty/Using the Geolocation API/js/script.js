c0d387a1116c43b5f7af4985a4db2784ff339322
Numonof <numonofcv@gmail.com>
2025-05-02 15:58:52 +0300

    Displaying a Map Marker

--- Mapty/Using the Geolocation API/js/script.js
+++ Mapty/Using the Geolocation API/js/script.js
@@ -32,10 +32,13 @@ if (navigator.geolocation) {
             const {latitude} = position.coords;
             const {longitude} = position.coords;
             console.log(`https://www.google.co.uk/maps/@${latitude},${longitude}`);
-const coords =[latitude,longitude]
+            
+            const coords = [latitude, longitude];
+            
             const map = L
                 .map('map')
                 .setView(coords, 13);
+            // console.log(map);
 
             L
                 .tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -44,11 +47,23 @@ const coords =[latitude,longitude]
                 })
                 .addTo(map);
 
-            L
-                .marker(coords)
-                .addTo(map)
-                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-                .openPopup();
+            map.on('click', function (mapEvent) {
+                console.log(mapEvent);
+                const {lat, lng} = mapEvent.latlng
+                 
+                    L
+                    .marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(L.popup({
+                        maxWidth:250,
+                        minWidth:100,
+                        autoClose:false, 
+                        closeClick:false,
+                        className:'running-popup'
+                    }))
+                    .setPopupContent('Workout')
+                    .openPopup();
+            });
         }, function () {
             alert('Could not get your position')
         });
